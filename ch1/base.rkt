a176e8d133f2178ba2ed2a91121dc260d1e5edab
Kevin Smith <ksmithbaylor@gmail.com>
2015-02-27 14:36:09 -0500

    complete exercises 1.5, 1.6, and 1.7

--- ch1/base.rkt
+++ ch1/base.rkt
@@ -23,3 +23,20 @@
 
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
--- ch1/exercises.rkt
+++ ch1/exercises.rkt
@@ -8,10 +8,12 @@
 (define 1-1
   (list 10 12 8 3 6 3 4 19 #f 4 16 6 16))
 
+
 ;;; Exercise 1.2 ;;;
 (define 1-2
   (/ (+ 5 4 (- 2 (- 3 (+ 6 4/5))))
      (* 3 (- 6 2) (- 2 7))))
+
 
 ;;; Exercise 1.3 ;;;
 (define (1-3 a b c)
@@ -19,6 +21,77 @@
     (sum-of-squares a (max b c))
     (sum-of-squares b (max a c))))
 
+
 ;;; Exercise 1.4 ;;;
 ; If b is positive, it will be added to a.
 ; If b is negative, it will be subtracted from a.
+
+
+;;; Exercise 1.5 ;;;
+; If the interpreter is using applicative-order execution,
+; the procedure will never terminate. If the interpreter
+; is using normal-order execution, the procedure will
+; terminate very quickly and return a value of 0.
+;
+; This happens because `p` is defined as an infinitely-recursive
+; procedure. When all operands are evaluated first before
+; evaluating the expression as a whole, the interpreter
+; tries to evaluate `(p)` but gets "stuck". When the
+; expression is expanded to "thunks" of un-evaluated code,
+; the `test` procedure evaluates to 0 and never has to
+; evaluate `(p)`.
+
+
+;;; Exercise 1.6 ;;;
+; The program will never terminate. Similar to the previous
+; example, since the interpreter uses applicative-order
+; execution, it will attempt to evaluate the else-clause
+; regardless of the value of the predicate. Since the
+; else-clause in this example is a recursive call, the
+; interpreter will get "stuck" again in an infinite call
+; stack.
+
+
+;;; Exercise 1.7 ;;;
+; Since the interval we define as "good enough" stays constant
+; and does not scale with the size of `x`, it is proportionally
+; bigger for small numbers, leaving a bigger margin for error.
+;
+; For example, consider the following examples:
+; > (/ (sqrt 2.25) (- (sqrt 2.25) 1.5))
+; 195312.99999889117
+; > (/ (sqrt 10000) (- (sqrt 10000) 100))
+; 392299270.1834477
+;
+; This means that the result of our procedure applied to 2.25 is
+; inaccurate by one part in 195,313, since it is known that the
+; square root of 2.25 is 1.5. But the result of our procedure
+; applied to 10000 is only inaccurate by one part in 392,299,270,
+; since it is known that the square root of 10000 is 100.
+;
+; To fix this, we can define a procedure that tracks the change
+; between each successive guess and declares a guess "good enough"
+; when it has changed less than a small fraction of the guess
+; itself. Since the fraction is indirectly based on the size of `x`,
+; it can be said to scale well and produce more accurate results
+; for very small numbers.
+
+(define (1-7-sqrt x)
+  (1-7-sqrt-iter 1.0 0.0 x))
+
+(define (1-7-sqrt-iter guess prev-guess x)
+  (if (1-7-good-enough? guess prev-guess)
+    guess
+    (1-7-sqrt-iter (improve guess x) guess x)))
+
+(define (1-7-good-enough? guess prev-guess)
+  (> 0.001 (/ guess (- guess prev-guess))))
+
+; This improves the accuracy quite a bit. Using our new function:
+; > (/ (1-7-sqrt 2.25) (- (1-7-sqrt 2.25) 1.5))
+; 313.00000000000114
+; > (/ (1-7-sqrt 10000) (- (1-7-sqrt 10000) 100))
+; 1.0416449783088315
+;
+; The procedure is still more accurate for large numbers, but it is
+; orders of magnitude more accurate all around.
