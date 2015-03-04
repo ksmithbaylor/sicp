07c11316c67a98bc2e36f7560192b4364d48d606
Kevin Smith <ksmithbaylor@gmail.com>
2015-03-04 13:05:40 -0500

    completed exercise 1.9

--- ch1/exercises.rkt
+++ ch1/exercises.rkt
@@ -112,3 +112,43 @@
                  guess
                  guess)
               3)))
+
+;;; Exercise 1.9 ;;;
+; First
+; (define (+ a b)
+;   (if (= a 0)
+;     b
+;     (inc (+ (dec a)
+;             b))))
+;
+; Process
+; (+ 4 5)
+; (inc (+ 3 5))
+; (inc (inc (+ 2 5)))
+; (inc (inc (inc (+ 1 5))))
+; (inc (inc (inc (inc (+ 0 5)))))
+; (inc (inc (inc (inc 5))))
+; (inc (inc (inc 6)))
+; (inc (inc 7))
+; (inc 8)
+; 9
+;
+; The first procedure is recursive.
+;
+;
+; Second
+; (define (+ a b)
+;   (if (= a 0)
+;     b
+;     (+ (dec a)
+;        (inc b))))
+;
+; Process
+; (+ 4 5)
+; (+ 3 6)
+; (+ 2 7)
+; (+ 1 8)
+; (+ 0 9)
+; 9
+;
+; The second procedure is iterative.
